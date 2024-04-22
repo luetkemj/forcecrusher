dbae90723db44241175858a4acdee289556732d9
luetkemj <luetkemj@gmail.com>
2024-04-21 21:03:28 -0600

    wip

---
+++ src/ecs/engine.ts
@@ -0,0 +1,17 @@
+import { World } from "miniplex"
+
+type Entity = {
+  position?: { x: number; y: number; z: number };
+  tryMove?: { x: number; y: number; z: number };
+  appearance?: {
+    char: string;
+    tint: number;
+    tileSet: string;
+  }
+}
+
+export const start = () => {
+  const world = new World<Entity>()
+
+  return world
+}
