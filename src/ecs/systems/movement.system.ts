9c5df64663311b0de46a327342cf7e13da32c532
luetkemj <luetkemj@gmail.com>
2024-04-24 18:57:48 -0600

    disallow walking through walls

--- src/ecs/systems/movement.system.ts
+++ src/ecs/systems/movement.system.ts
@@ -15,15 +15,27 @@ import { world } from "../engine";
 // import { getState } from "../../main";
 
 const moveableEntities = world.with("position", "tryMove").without("paused");
+const blockingEntities = world.with("blocking", "position").without("paused");
 
 export const movementSystem = () => {
   for (const entity of moveableEntities) {
     const { position, tryMove } = entity;
+
+    for (const blocker of blockingEntities) {
+      if (
+        blocker.position.x === entity.tryMove.x &&
+        blocker.position.y === entity.tryMove.y
+      ) {
+        world.removeComponent(entity, "tryMove");
+        return console.log(`blocked by ${blocker.name}`);
+      } 
+    }
+
     position.x = tryMove.x;
     position.y = tryMove.y;
     position.z = tryMove.z;
 
-    world.removeComponent(entity, 'tryMove')
+    world.removeComponent(entity, "tryMove");
   }
 };
 
