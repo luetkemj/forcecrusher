190c0daee4e271d68266ed961d7b05c322e149f0
luetkemj <luetkemj@gmail.com>
2025-06-08 15:30:04 -0600

    refactor attack system

--- src/ecs/systems/attack.system.ts
+++ src/ecs/systems/attack.system.ts
@@ -1,64 +1,80 @@
 import { addLog } from "../../lib/utils";
-import { gameWorld, type Entity, type Attack, type Damage } from "../engine";
+import {
+  type IGameWorld,
+  type Entity,
+  type Attack,
+  type Damage,
+} from "../engine";
 import { sample } from "lodash";
 import { DiceRoll } from "@dice-roller/rpg-dice-roller";
 import { getArmorClass } from "../../lib/combat";
 
-const attackingEntities = gameWorld.world.with("attackTarget");
+export const createAttackSystem = (
+  world: IGameWorld["world"],
+  registry: IGameWorld["registry"],
+) => {
+  const attackQuery = world.with("attackTarget");
 
-export const attackSystem = () => {
-  for (const attacker of attackingEntities) {
-    const target = attacker.attackTarget;
+  return function attack() {
+    for (const attacker of attackQuery) {
+      const target = attacker.attackTarget;
 
-    let playerInCombat = false;
-    if (attacker.pc || target.pc) {
-      playerInCombat = true;
-    }
-
-    const weapon = getWeapon(attacker);
-    let attack = getAttack(attacker);
-
-    if (!attack) {
-      console.log(`${attacker.name} has no means of attack`);
-      return;
-    }
-
-    // roll attack
-    // TODO: should this have any toHit bonuses?
-    const attackRoll = new DiceRoll("d20").total;
-    const isCrit = attackRoll === 20;
-
-    const armorClass = getArmorClass(target) || 0;
-
-    if (!target.health) return;
-
-    if (attackRoll >= armorClass) {
-      // NOTE: HIT
-      let damages = calcAttackDamage(attacker, attack, target, isCrit, weapon);
-
-      if (target.damages) {
-        target.damages.push(...damages);
+      let playerInCombat = false;
+      if (attacker.pc || target.pc) {
+        playerInCombat = true;
       }
-    } else {
-      // NOTE: MISS
-      if (playerInCombat) {
-        addLog(`${attacker.name} misses ${target.name}!`);
-      }
-    }
 
-    gameWorld.world.removeComponent(attacker, "attackTarget");
-  }
+      const weapon = getWeapon(attacker, registry);
+      let attack = getAttack(attacker, registry);
+
+      if (!attack) {
+        console.log(`${attacker.name} has no means of attack`);
+        return;
+      }
+
+      // roll attack
+      // TODO: should this have any toHit bonuses?
+      const attackRoll = new DiceRoll("d20").total;
+      const isCrit = attackRoll === 20;
+
+      const armorClass = getArmorClass(target) || 0;
+
+      if (!target.health) return;
+
+      if (attackRoll >= armorClass) {
+        // NOTE: HIT
+        let damages = calcAttackDamage(
+          attacker,
+          attack,
+          target,
+          isCrit,
+          weapon,
+        );
+
+        if (target.damages) {
+          target.damages.push(...damages);
+        }
+      } else {
+        // NOTE: MISS
+        if (playerInCombat) {
+          addLog(`${attacker.name} misses ${target.name}!`);
+        }
+      }
+
+      world.removeComponent(attacker, "attackTarget");
+    }
+  };
 };
 
-function getWeapon(entity: Entity) {
+function getWeapon(entity: Entity, registry: IGameWorld["registry"]) {
   const weaponId = entity.weaponSlot?.contents[0];
   if (weaponId) {
-    return gameWorld.registry.get(weaponId);
+    return registry.get(weaponId);
   }
 }
 
-function getAttack(entity: Entity) {
-  const weapon = getWeapon(entity);
+function getAttack(entity: Entity, registry: IGameWorld["registry"]) {
+  const weapon = getWeapon(entity, registry);
   let attack;
   if (weapon) {
     attack = sample(weapon.attacks);
