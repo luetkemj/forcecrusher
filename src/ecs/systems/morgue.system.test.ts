87fc7ad0a8939dd15a4b3001495be2f91d56c03a
luetkemj <luetkemj@gmail.com>
2025-06-11 07:48:32 -0600

    Create morgue.system.test.ts

---
+++ src/ecs/systems/morgue.system.test.ts
@@ -0,0 +1,50 @@
+import { describe, test, expect, beforeEach } from "vitest";
+import type { Entity, IGameWorld } from "../engine";
+import { setupTestGameWorld } from "./test-utils";
+import { createMorgueSystem } from "./morgue.system";
+import { getState, GameState } from "../gameState";
+
+describe("morgue.system", () => {
+  let gameWorld: IGameWorld;
+  let entity: Entity;
+  beforeEach(() => {
+    gameWorld = setupTestGameWorld();
+    entity = {
+      id: "1",
+      name: "Skeleton",
+      version: 1,
+      health: { max: 10, current: 1 },
+      appearance: { char: "S", tint: 0xffffff, tileSet: "default" },
+    };
+    gameWorld.world.add(entity);
+  });
+
+  test("marks entity as dead and changes components when health <= 0", () => {
+    entity.health && (entity.health.current = 0);
+    createMorgueSystem(gameWorld.world)();
+    expect(entity.appearance?.char).toBe("%");
+    expect(entity.dead).toBe(true);
+    expect(entity.layer200).toBe(true);
+    expect(entity.layer300).toBeUndefined();
+    expect(entity.pickUp).toBe(true);
+    const { log } = getState();
+    expect(log[log.length - 1]).toBe("Skeleton has died!");
+  });
+
+  test("sets game over if player dies", () => {
+    if (entity.health) entity.health.current = 0;
+    entity.name = "player";
+    entity.pc = true;
+    createMorgueSystem(gameWorld.world)();
+    expect(getState().gameState).toBe(GameState.GAME_OVER);
+    const { log } = getState();
+    expect(log[log.length - 1]).toBe("player has died!");
+  });
+
+  test("does nothing if entity is alive", () => {
+    if (entity.health) entity.health.current = 5;
+    createMorgueSystem(gameWorld.world)();
+    expect(entity.dead).toBeUndefined();
+    expect(entity.appearance?.char).toBe("S");
+  });
+});
