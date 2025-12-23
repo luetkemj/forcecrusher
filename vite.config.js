62d48c88ca1b378cb31a6e189e0eede207479f40
Mark Luetke <luetkemj@gmail.com>
2025-12-23 16:44:13 -0700

    add script via vite build (#51)

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,26 @@
-// vite.config.js
-export default {
-    base: "/forcecrusher/",
-};
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/forcecrusher/",
+  plugins: [
+    {
+      name: "add-analytics-script",
+      apply: "build", // ensures the plugin runs only during the build command
+      transformIndexHtml() {
+        return {
+          tags: [
+            {
+              tag: "script",
+              attrs: {
+                async: true,
+                src: "https://cloud.umami.is/script.js",
+                "data-website-id": "830512db-3c4e-43ab-bc78-1785bc87ef2f",
+              },
+              injectTo: "head",
+            },
+          ],
+        };
+      },
+    },
+  ],
+});
