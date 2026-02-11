61b6333cdccf4d6f521f14cd3e224959db666e4b
luetkemj <luetkemj@gmail.com>
2026-02-11 16:28:20 -0700

    fix game mode

--- src/ecs/inputHandlers/handleScreenTitleMode.ts
+++ src/ecs/inputHandlers/handleScreenTitleMode.ts
@@ -3,8 +3,7 @@ import { GameState, State } from "../gameState";
 
 export const handleScreenTitleMode = ({ key, setState }: InputContext) => {
   if (key) {
-    setState((state: State) => (state.gameState = GameState.GAME));
+    setState((state: State) => (state.gameState = GameState.SIM));
     return true;
   }
-  return true;
 };
