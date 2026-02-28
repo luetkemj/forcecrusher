64a64570390f87ea847be8f02859fcbe50cb16d2
luetkemj <luetkemj@gmail.com>
2026-02-28 06:45:07 -0700

    refactor attacks

---
+++ src/attacks/index.ts
@@ -0,0 +1,18 @@
+import {
+  bash,
+  beak,
+  bite,
+  claw,
+  kick,
+  lavaPunch,
+  slash,
+  stab,
+  stomp,
+} from "./melee";
+
+export default {
+  melee: { kick, bite, claw, lavaPunch, beak, bash, stomp, stab, slash },
+  ranged: {},
+  meleeSpell: {},
+  rangedSpell: {},
+};
