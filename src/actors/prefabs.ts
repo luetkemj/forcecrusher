3e76154b4d3bae23b7d052be85882a3c47ee4878
luetkemj <luetkemj@gmail.com>
2026-03-05 19:13:59 -0700

    remove unused

--- src/actors/prefabs.ts
+++ src/actors/prefabs.ts
@@ -11,13 +11,6 @@ import {
   ReadableType,
   TileSet,
 } from "../ecs/enums";
-import { createBlood } from "../spells/spellbook/createBlood";
-import { createWater } from "../spells/spellbook/createWater";
-import { desiccate } from "../spells/spellbook/desiccate";
-import { ignite } from "../spells/spellbook/ignite";
-import { kill } from "../spells/spellbook/kill";
-import { knock } from "../spells/spellbook/knock";
-import { massKill } from "../spells/spellbook/massKill";
 import { colors, chars } from "./graphics";
 
 export const fluidContainerComponent = {
